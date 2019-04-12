9b221a6e0b7f3cea68923758d8279f327da31135
XBean1024 <596928539@qq.com>
2019-04-12 15:53:35 +0800

    网络请求重连次数设置为1

--- app/src/main/java/org/ionchain/wallet/App.java
+++ app/src/main/java/org/ionchain/wallet/App.java
@@ -10,12 +10,9 @@ import com.facebook.stetho.Stetho;
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.cache.CacheEntity;
 import com.lzy.okgo.cache.CacheMode;
-import com.lzy.okgo.cookie.CookieJarImpl;
-import com.lzy.okgo.cookie.store.DBCookieStore;
-import com.lzy.okgo.https.HttpsUtils;
 import com.lzy.okgo.interceptor.HttpLoggingInterceptor;
-import org.ionc.qrcode.activity.ZXingLibrary;
 
+import org.ionc.qrcode.activity.ZXingLibrary;
 import org.ionc.wallet.sdk.IONCWalletSDK;
 import org.ionchain.wallet.crasher.CrashHandler;
 
@@ -72,24 +69,9 @@ public class App extends Application {
         //builder.addInterceptor(new ChuckInterceptor(this));
 
         //超时时间设置，默认60秒
-        builder.readTimeout(3, TimeUnit.SECONDS);      //全局的读取超时时间
+        builder.readTimeout(5, TimeUnit.SECONDS);      //全局的读取超时时间
         builder.writeTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);     //全局的写入超时时间
-        builder.connectTimeout(3, TimeUnit.SECONDS);   //全局的连接超时时间
-
-        //自动管理cookie（或者叫session的保持），以下几种任选其一就行
-        //builder.cookieJar(new CookieJarImpl(new SPCookieStore(this)));            //使用sp保持cookie，如果cookie不过期，则一直有效
-        builder.cookieJar(new CookieJarImpl(new DBCookieStore(this)));              //使用数据库保持cookie，如果cookie不过期，则一直有效
-        //builder.cookieJar(new CookieJarImpl(new MemoryCookieStore()));            //使用内存保持cookie，app退出后，cookie消失
-
-        //https相关设置，以下几种方案根据需要自己设置
-        //方法一：信任所有证书,不安全有风险
-        HttpsUtils.SSLParams sslParams1 = HttpsUtils.getSslSocketFactory();
-        //方法二：自定义信任规则，校验服务端证书
-        //方法三：使用预埋证书，校验服务端证书（自签名证书）
-        //HttpsUtils.SSLParams sslParams3 = HttpsUtils.getSslSocketFactory(getAssets().open("srca.cer"));
-        //方法四：使用bks证书和密码管理客户端证书（双向认证），使用预埋证书，校验服务端证书（自签名证书）
-        //HttpsUtils.SSLParams sslParams4 = HttpsUtils.getSslSocketFactory(getAssets().open("xxx.bks"), "123456", getAssets().open("yyy.cer"));
-        builder.sslSocketFactory(sslParams1.sSLSocketFactory, sslParams1.trustManager);
+        builder.connectTimeout(5, TimeUnit.SECONDS);   //全局的连接超时时间
 
 
         // 其他统一的配置
@@ -98,7 +80,7 @@ public class App extends Application {
                 .setOkHttpClient(builder.build())               //建议设置OkHttpClient，不设置会使用默认的
                 .setCacheMode(CacheMode.NO_CACHE)               //全局统一缓存模式，默认不使用缓存，可以不传
                 .setCacheTime(CacheEntity.CACHE_NEVER_EXPIRE)   //全局统一缓存时间，默认永不过期，可以不传
-                .setRetryCount(3);                            //全局统一超时重连次数，默认为三次，那么最差的情况会请求4次(一次原始请求，三次重连请求)，不需要可以设置为0
+                .setRetryCount(1);                            //全局统一超时重连次数，默认为三次，那么最差的情况会请求4次(一次原始请求，三次重连请求)，不需要可以设置为0
 
 
     }
