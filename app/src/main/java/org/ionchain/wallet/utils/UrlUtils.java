59d643ed0ef020bb477c8ace1ba0d323ffe07d4b
XBean1024 <596928539@qq.com>
2019-06-14 15:40:36 +0800

    修复部分bug

--- app/src/main/java/org/ionchain/wallet/utils/UrlUtils.java
+++ app/src/main/java/org/ionchain/wallet/utils/UrlUtils.java
@@ -4,22 +4,25 @@ import org.ionchain.wallet.BuildConfig;
 import org.ionchain.wallet.constant.ConstantUrl;
 
 public class UrlUtils {
+    private static boolean debug = BuildConfig.DEBUG;
+//    private static boolean debug = false;
     /**
      * @return 获取主链节点
      */
     public static String getNodeListUrl() {
-        if (BuildConfig.DEBUG) {
+        if (debug) {
             return ConstantUrl.URL_NODE_LIST_DEBUG;
         } else {
             return ConstantUrl.URL_NODE_LIST;
         }
+        
     }
 
     /**
      * @return 钱包更新接口
      */
     public static String getUpdateApkUrl() {
-        if (BuildConfig.DEBUG) {
+        if (debug) {
             return ConstantUrl.URL_UPDATE_APK_DEBUG;
         } else {
             return ConstantUrl.URL_UPDATE_APK;
@@ -31,7 +34,7 @@ public class UrlUtils {
      * @return 交易记录接口
      */
     public static String getTxRecordUrl() {
-        if (BuildConfig.DEBUG) {
+        if (debug) {
             return ConstantUrl.URL_TX_RECORD_GET_DEBUG;
         } else {
             return ConstantUrl.URL_TX_RECORD_GET;
@@ -42,7 +45,7 @@ public class UrlUtils {
      * @return 获取美元价格
      */
     public static String getUSDPriceUrl() {
-        if (BuildConfig.DEBUG) {
+        if (debug) {
             return ConstantUrl.URL_USD_PRICE_DEBUG;
         } else {
             return ConstantUrl.URL_USD_PRICE;
@@ -53,7 +56,7 @@ public class UrlUtils {
      * @return 获取美元-人民币汇率信息接口
      */
     public static String getUSDExRateRMBUrl() {
-        if (BuildConfig.DEBUG) {
+        if (debug) {
             return ConstantUrl.URL_USD_EX_RATE_RMB_PRICE_DEBUG;
         } else {
             return ConstantUrl.URL_USD_EX_RATE_RMB_PRICE;
