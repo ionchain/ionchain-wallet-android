e7050ed8031318f6727ee22e384f55e6ac69a09a
XBean1024 <596928539@qq.com>
2019-06-21 14:04:06 +0800

    https

--- app/src/main/java/org/ionchain/wallet/constant/ConstantUrl.java
+++ app/src/main/java/org/ionchain/wallet/constant/ConstantUrl.java
@@ -44,10 +44,10 @@ public class ConstantUrl {
 
     private final static String HOST_PRICE = "http://explorer.ionchain.org";
     public final static String URL_USD_PRICE = HOST_PRICE + "/v1/version";   //美元价格
-    public final static String URL_USD_EX_RATE_RMB_PRICE = HOST_PRICE + "/v1/currency";   //人民币兑美元汇率
+    public final static String URL_USD_EX_RATE_RMB_PRICE = HOST_PRICE + "/v2/currency";   //人民币兑美元汇率
     public final static String URL_TX_RECORD_GET = HOST_PRICE + "/v1/transaction";   //交易记录
 
-    public final static String HOST_NODE_MAIN = "http://api.ionchain.org";
+    public final static String HOST_NODE_MAIN = "https://api.ionchain.org";
 
 
     //备用节点
